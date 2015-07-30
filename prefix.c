ef0ce3618b71c49afa6905fc00bd992aa52e63ac
nyux <emilia.emerald@gmail.com>
2015-07-30 06:31:41 -0400

    added tentative data structures

--- prefix.c
+++ prefix.c
@@ -5,10 +5,37 @@
 
 #include <editline/readline.h>
 
+/* current ideas of what data structures i need:
+ * a node_t for the linked list that is the stack
+ * a value_t for whether a token is a double or malformed
+ */
+
+struct node {
+    double value;
+    struct node *next;
+};
+
+struct value {
+    bool is_valid;
+    double value; /* if invalid input, NAN */
+};
+
+typedef struct node node_t;
+typedef struct value value_t ;
+
 bool should_quit(char *input) {
     return!strcmp(input, "q") || !strcmp(input, "quit") || !strcmp(input, "Q");
 }
 
+/* current plan:
+ *  - parse the operator
+ *  - skip ahead to the next token
+ *  - test if is a parenthesis
+ *      - if so, duplicate the string up until the end of the closing paren
+ *      - run calculate again
+ *  - if is a number, add to stack
+ *  - once you reach the end of the string, do calculation and return result
+ */
 double calculate(char *str_to_parse) {
     return 0.0;
 }
