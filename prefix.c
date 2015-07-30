6aff74bef4d1a4136e6f12830c7ff8412f1932d7
nyux <emilia.emerald@gmail.com>
2015-07-30 16:31:45 -0400

    added... pretty much everything. it works now, though it's fragile. thing to learn for the future: how to commit in small chunks when in the moment

--- prefix.c
+++ prefix.c
@@ -19,7 +19,7 @@ bool should_quit(char *input)
     return!strcmp(input, "q") || !strcmp(input, "quit") || !strcmp(input, "Q");
 }
 
-operator_t get_operation(char *operator_str)
+operator_t get_operator(char *operator_str)
 {
     if (strcmp(operator_str, "+") == 0)
     {
@@ -45,7 +45,82 @@ double subtract(double x, double y) { return x - y; }
 double multiply(double x, double y) { return x * y; }
 double divide(double x, double y) { return x / y; }
 
-double unroll_stack() { return 0.0; }
+double unroll_queue(queue_t *queue, operator_t operator) 
+{
+    /* assumption: the queue will have at least two items in it */
+    double x = pop(queue), y, temp;
+    do
+    {
+        y = pop(queue);
+        temp = (*operator.fn)(x, y);
+        x = temp;
+    } while (queue->head);
+
+    free(queue);
+    return x;
+}
+
+subexpression_t get_subexpression(char *str)
+{
+    int level = 1;
+    char *str_copy = str;
+    char *subexpression;
+
+    str_copy++; /* move the pointer away from the leading '(' */
+
+    while (level > 0)
+    {
+        if (*str_copy == '(') level++;
+        else if (*str_copy == ')') level--;
+        str_copy++;
+    }
+
+    /* assumes the character after the final ) is a space */
+    *str_copy = '\0';
+    subexpression = strdup(str);
+    *str_copy = ' ';
+
+    return new_subexpression_t(.subexpr = subexpression, .end_of_subexpr = str_copy);
+}
+
+queue_t* create_queue()
+{
+    queue_t *queue = malloc(sizeof(queue_t));
+    queue->head = NULL;
+    queue->tail = NULL;
+    return queue;
+}
+
+node_t* create_node(node_t *next, double value)
+{
+    node_t *node = malloc(sizeof(node_t));
+    node->next = next;
+    node->value = value;
+    return node;
+}
+
+void push(queue_t *queue, double value)
+{
+    node_t *temp = create_node(NULL, value);
+    
+    if (!queue->head) queue->head = temp;
+   
+    if (queue->tail) queue->tail->next = temp;
+    else queue->tail = temp;
+  
+    queue->tail = temp;
+}
+
+double pop(queue_t *queue)
+{
+    node_t *current_head = queue->head;
+    double value = current_head->value;
+
+    queue->head = current_head->next;
+    free(current_head);
+
+    return value;
+}
 
 /* current plan:
  *  - parse the operator
@@ -56,48 +131,88 @@ double unroll_stack() { return 0.0; }
  *  - if is a number, add to stack
  *  - once you reach the end of the string, do calculation and return result
  */
-double calculate(char *str_to_parse)
+double calculate(char *expression)
 {
+    if (*expression == '(') expression++;
+    
     char *saveptr;
-    char *operation_str = strtok_r(str_to_parse, " ", &saveptr);
-    char *token;
+    char *token = strtok_r(expression, " ", &saveptr);
+    queue_t *queue = create_queue();
 
-    operator_t operation = get_operation(operation_str);
+    operator_t operator = get_operator(token);
 
-    if (!operation.is_valid)
+    if (!operator.is_valid)
     {
-        free(str_to_parse);
-        puts("invalid input");
+        free(saveptr);
+        free(queue);
+        puts("invalid input; terminating program");
         exit(-1);
     }
 
     while ((token = strtok_r(NULL, " ", &saveptr)))
     {
-        break;
+        begin:
+        if (token == NULL) break;
+        else if (*token == '(')
+        {
+            token++;
+            char *undo_nullterminator = token;
+            do { undo_nullterminator++; } while (*undo_nullterminator);
+            *undo_nullterminator = ' ';
+
+            subexpression_t subexpression = get_subexpression(token);
+            push(queue, calculate(subexpression.subexpr));
+
+            expression = subexpression.end_of_subexpr;
+            saveptr = NULL;
+            
+            free(subexpression.subexpr);
+
+            token = strtok_r(expression, " ", &saveptr);
+            goto begin;
+        }
+        else
+        {
+            char *end;
+            double value = strtod(token, &end);
+
+            if (value == 0.0 && end == token)
+            {
+                puts("invalid input, exiting program");
+                exit(-1);
+            }
+            else push(queue, value);
+
+            if (*end == ')') break;
+        }
     }
 
-    unroll_stack(/* blah blah blah */);
-
-
-    return 0.0;
+    return unroll_queue(queue, operator);
 }
 
 int main(void)
 {
     char *input = "";
 
+    puts("This program's a bit fragile, and only does okay with well-formatted input.");
+
     while (true)
     {
         input = readline("calc> ");
 
-        if (should_quit(input))
+        if (!input)
+        {
+            puts("");
+            break;
+        }
+        else if (should_quit(input))
         {
             free(input);
             break;
         }
 
         add_history(input);
-        printf("%lf\n", calculate(input));
+        printf("%.3lf\n", calculate(input));
         free(input);
     }
 } 
--- prefix.h
+++ prefix.h
@@ -1,32 +1,47 @@
 #pragma once
 
-struct node {
-    double value;
+struct node 
+{
     struct node *next;
+    double value;
 };
 
-struct value {
-    bool is_valid;
-    double value; /* if invalid input, NAN */
+struct queue
+{
+    struct node *head;
+    struct node *tail;
 };
 
-struct operator {
+struct operator 
+{
     bool is_valid;
     int num_operands; /* if invalid, -1 */
     double (*fn)(double x, double y); /* if invalid, NULL */
 };
 
+struct subexpression
+{
+    char *subexpr;
+    char *end_of_subexpr;
+};
+
 typedef struct node node_t;
-typedef struct value value_t ;
+typedef struct queue queue_t;
 typedef struct operator operator_t;
+typedef struct subexpression subexpression_t;
 
 #define new_operator_t(...) (operator_t) { __VA_ARGS__ }
+#define new_subexpression_t(...) (subexpression_t) { __VA_ARGS__ }
 
 bool should_quit(char *input);
-operator_t get_operation(char *operator_str);
+operator_t get_operator(char *operator_str);
 double calculate(char *str_to_parse);
-double unroll_stack(void);
-
+double unroll_queue(queue_t *queue, operator_t operator);
+subexpression_t get_subexpression(char *subexpression);
+queue_t* create_queue(void);
+node_t* create_node(node_t *next, double value);
+void push(queue_t *queue, double value);
+double pop(queue_t *queue);
 
 double add(double x, double y) ;
 double subtract(double x, double y) ;
